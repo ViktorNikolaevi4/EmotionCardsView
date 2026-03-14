9f7b61c87ef25a47414bf59ec86c71525943c01b
ViktorNikolaevi4 <v87v87@icloud.com>
2026-03-14 22:38:21 +0300

    Initial Commit

---
+++ EmotionCardsView/EmotionCardsViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionCardsViewApp.swift
+//  EmotionCardsView
+//
+//  Created by Виктор Корольков on 14.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct EmotionCardsViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
